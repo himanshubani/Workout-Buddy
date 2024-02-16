49bda9b1856f73247cd6106e14d2bc8b25cc210a
himanshu <himanshu.bani123@gmail.com>
2024-02-16 14:00:44 +0530

    Fix authorization issue in WorkoutForm and WorkoutDetails components

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -2,7 +2,7 @@ import { createContext, useReducer, useEffect } from 'react'
 
 export const AuthContext = createContext()
 
-export const authReducer = (state, action) => {
+const authReducer = (state, action) => {
   switch (action.type) {
     case 'LOGIN':
       return { user: action.payload }

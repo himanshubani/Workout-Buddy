64fbc12504602c16d1b20d8590b9f4ede5cfcc69
himanshu <himanshu.bani123@gmail.com>
2024-02-16 23:15:59 +0530

    Add dispatch for workouts context in useLogout hook

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,12 +1,15 @@
 import { useAuthContext } from "./useAuthContext";
+import { useWorkoutsContext } from "./useWorkoutsContext";
 
 export const useLogout = () => {
   const { dispatch } = useAuthContext();
+  const { dispatch: dispatchWorkouts } = useWorkoutsContext();
 
   const logout = () => {
     // remove the user from local storage
     localStorage.removeItem("user");
     dispatch({ type: "LOGOUT" });
+    dispatchWorkouts({ type: "SET_WORKOUTS", payload: null});
   };
 
   return {logout};
